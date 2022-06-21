4a9fad82f7f8f083b8139421cac36d256e5ccf09
dymafr <erwan1309@hotmail.com>
2022-06-21 13:14:27 +0200

    2 files modified

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Recipe.module.scss';
 
 function Recipe({ title, image }) {
+  const [liked, setLiked] = useState(false);
+
+  function handleClick() {
+    setLiked(!liked);
+  }
+
   return (
-    <div className={styles.recipe}>
+    <div onClick={handleClick} className={styles.recipe}>
       <div className={styles.imageContainer}>
         <img src={image} alt={title} />
       </div>
@@ -11,7 +17,7 @@ function Recipe({ title, image }) {
         className={`${styles.recipeTitle} d-flex flex-column justify-content-center align-items-center`}
       >
         <h3 className="mb-10">{title}</h3>
-        <i className={`fa-solid fa-heart`}></i>
+        <i className={`fa-solid fa-heart ${liked ? 'text-primary' : ''}`}></i>
       </div>
     </div>
   );
