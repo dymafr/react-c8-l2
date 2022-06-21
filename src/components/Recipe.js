803f6bfe511a3c59a85895dabf238e65735bd0e8
dymafr <erwan1309@hotmail.com>
2022-06-21 13:05:48 +0200

    2 files modified

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import styles from './Recipe.module.scss';
 
-function Recipe() {
+function Recipe({ title, image }) {
   return (
     <div className={styles.recipe}>
       <div className={styles.imageContainer}>
-        <img
-          src="https://github.com/dymafr/react-chapitre05-cookchef-partie1/blob/master/src/assets/images/recette.jpg?raw=true"
-          alt="recipe"
-        />
+        <img src={image} alt={title} />
       </div>
       <div
         className={`${styles.recipeTitle} d-flex flex-row justify-content-center align-items-center`}
       >
-        <h3>Saumon et asperges</h3>
+        <h3>{title}</h3>
       </div>
     </div>
   );
