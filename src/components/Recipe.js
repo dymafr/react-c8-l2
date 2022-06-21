4eb6ba8586459f2b6084a1071bf4a4da3c404b06
dymafr <erwan1309@hotmail.com>
2022-06-21 13:11:43 +0200

    2 files modified

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -8,9 +8,10 @@ function Recipe({ title, image }) {
         <img src={image} alt={title} />
       </div>
       <div
-        className={`${styles.recipeTitle} d-flex flex-row justify-content-center align-items-center`}
+        className={`${styles.recipeTitle} d-flex flex-column justify-content-center align-items-center`}
       >
-        <h3>{title}</h3>
+        <h3 className="mb-10">{title}</h3>
+        <i className={`fa-solid fa-heart`}></i>
       </div>
     </div>
   );
